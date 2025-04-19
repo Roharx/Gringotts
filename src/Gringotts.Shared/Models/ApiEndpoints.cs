a8f3b9a8c7ab9c63a46c09ccafff063d977deb59
Roharx <balint.imal@gmail.com>
2025-04-19 09:25:11 +0200

    Added rest of the files

--- src/Gringotts.Shared/Models/ApiEndpoints.cs
+++ src/Gringotts.Shared/Models/ApiEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿namespace Gringotts.Shared.Models;
 
-public class ApiEndpoints
+public static class ApiEndpoints
 {
-    
+    public const string CurrencyServiceBase = "http://currencyservice:8080/api/currency";
+    public const string LedgerServiceBase = "http://ledgerservice:8080/api/transactions";
 }
